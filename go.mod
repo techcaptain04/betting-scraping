eb68c26925db7e73d23b447e43837c221a253d8e
techcaptain04 <andrii.seliuk@outlook.com>
2024-03-20 18:56:41 -0700

    add sms package

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/ferretcode-freelancing/sportsbook-scraper
 
 go 1.21.0
+
+require gorm.io/gorm v1.25.8
+
+require (
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/twilio/twilio-go v1.19.0 // indirect
+)
--- sms/sms.go
+++ sms/sms.go
@@ -1,1 +1,82 @@
 package sms
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/twilio/twilio-go"
+	api "github.com/twilio/twilio-go/rest/api/v2010"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	PhoneNumber string
+}
+
+type SMS struct {
+	PhoneNumber  string
+	TwilioClient twilio.RestClient
+	Logger       slog.Logger
+	Recipients   []string
+}
+
+func NewSMS(db *gorm.DB) (SMS, error) {
+	sms := SMS{
+		PhoneNumber:  os.Getenv("TWILIO_PHONE_NUMBER"),
+		TwilioClient: *twilio.NewRestClient(),
+		Logger:       *slog.New(slog.NewJSONHandler(os.Stdout, nil)),
+	}
+
+	recipients, err := sms.GetRecipients(db)
+
+	if err != nil {
+		return sms, err
+	}
+
+	sms.Recipients = recipients
+
+	return sms, nil
+}
+
+func (s *SMS) SendSMS(siteURL string, teams []string, odds []float64) error {
+	params := &api.CreateMessageParams{}
+
+	params.SetBody(
+		fmt.Sprintf(
+			"New discrepancy detected on %s. The odds are %f - %f for teams %s and %s.",
+			siteURL,
+			odds[0],
+			odds[1],
+			teams[0],
+			teams[1],
+		),
+	)
+	params.SetFrom(s.PhoneNumber)
+
+	for _, r := range s.Recipients {
+		params.SetTo(r)
+
+		res, err := s.TwilioClient.Api.CreateMessage(params)
+
+		if err != nil {
+			return err
+		}
+
+		s.Logger.Info("SMS message sent to", "number", r, "sid", res.Sid)
+	}
+
+	return nil
+}
+
+func (s *SMS) GetRecipients(db *gorm.DB) ([]string, error) {
+	var users []User
+
+	err := db.Model(&User{}).Find(&users).Error
+
+	if err != nil {
+		return []string{}, err
+	}
+
+	return []string{}, nil
+}
